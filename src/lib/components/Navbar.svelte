c6e3cadb7bce1a8144eedab97a27a856a740f607
Tibet Tornaci <oofdere@hey.com>
2023-09-09 14:21:30 -0700

    add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { AppBar, ProgressBar, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { wk, user } from '$lib/wkapi';
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<strong class="text-xl">wanwan <span class="chip variant-filled">alpha</span></strong>
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		{#await $user}
+			<ProgressRadial class="w-8" stroke={100} />
+		{:then user}
+			{#if user}
+				<div class="flex flex-col gap-2">
+					{user.data.username} • level {user.data.level}
+					<progress max={60} value={user.data.level} />
+				</div>
+			{/if}
+		{/await}
+	</svelte:fragment>
+</AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
@@ -7,37 +8,7 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl">wanwan <span class="chip variant-filled">alpha</span></strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<Navbar />
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
